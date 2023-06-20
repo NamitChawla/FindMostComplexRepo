005652c4b28e2376b653d8b138cd2f7e6bc446a6
Namit Chawla <60170131+NamitChawla@users.noreply.github.com>
2023-06-20 23:01:41 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-openai_key="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"
+openai_key="YOUR KEY HERE"
 max_file_size = 10 * 1024 * 1024  # 10 MB
 max_chunk_size = 4096  # Maximum tokens for GPT
